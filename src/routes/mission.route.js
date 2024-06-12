097c0103c9c8b13e1aefe15759cb6015b41e0522
hyouzl <hyouzl0521@gmail.com>
2024-06-12 16:38:21 +0900

    #1 [FEAT] 가게 리뷰 작성, 가게 미션 등록, 미션 도전하기 기능

--- src/routes/mission.route.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import asyncHandler from 'express-async-handler';
-import { newMemberMission } from '../controllers/mission.controller.js';
-
-
-export const missionRouter = express.Router();
-
-missionRouter.get('/member-mission', asyncHandler(newMemberMission));
