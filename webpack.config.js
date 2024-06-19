7af04027055e0d32e5dbf1da4a9eedd8208683d9
hyouzl <hyouzl0521@gmail.com>
2024-06-19 21:19:25 +0900

    webpack config 수정

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import nodeExternals from 'webpack-node-externals';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
