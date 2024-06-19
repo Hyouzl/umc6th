6e274192be9fb3adcb67731eef43f0f4cfc22fa9
hyouzl <hyouzl0521@gmail.com>
2024-06-19 21:16:23 +0900

    webpack config 수정

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,36 @@
-import nodeExternals from 'webpack-node-externals';
-import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default {
-  mode: 'development',
-  context: path.resolve(__dirname, 'src'),
-  entry: {
-    app: '../index.js',
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
+    mode: 'development',
+    context: resolve(__dirname, 'src'),
+    entry: {
+      app: '../index.js',
+    },
+    output: {
+      path: resolve(__dirname, 'dist'),
+      filename: 'main.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
           },
+          exclude: /node_modules/,
         },
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  target: 'node',
-  externalsPresets: {
-    node: true,
-  },
-  externals: [nodeExternals()],
-};
+      ],
+    },
+    target: 'node',
+    externalsPresets: {
+      node: true,
+    },
+    externals: [nodeExternals()],
+  };
